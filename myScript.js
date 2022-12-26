7bd19b7ab2d0c8e514083ae81d437070abc3728d
Shashank <shashankpandey2001@gmail.com>
2022-12-26 23:05:40 +0530

    reviews added

--- myScript.js
+++ myScript.js
@@ -1,16 +1,14 @@
+//Page Front Slide-Show
 let slideIndex = 1;
 showSlides(slideIndex);
-
 // Next/previous controls
 function plusSlides(n) {
   showSlides((slideIndex += n));
 }
-
 // Thumbnail image controls
 function currentSlide(n) {
   showSlides((slideIndex = n));
 }
-
 function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
@@ -30,8 +28,29 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+//Dark Mode Implementation
 (function () {
   document.querySelector(".dark-mode-button").addEventListener("click", () => {
-    document.body.classList.toggle("light-mode");
+    document.body.classList.toggle("dark-mode");
   });
 })();
+//Dark Mode Implementation Ends
+
+//Slides for Reviews
+let slides = document.querySelectorAll(".slide-container");
+let reviewIndex = 0;
+
+function next() {
+  slides[reviewIndex].classList.remove("active");
+  reviewIndex = (reviewIndex + 1) % slides.length;
+  slides[reviewIndex].classList.add("active");
+}
+function prev() {
+  slides[reviewIndex].classList.remove("active");
+  if (reviewIndex == 0) {
+    reviewIndex = slides.length;
+  }
+  reviewIndex = (reviewIndex - 1) % slides.length;
+  slides[reviewIndex].classList.add("active");
+}
+//Slides for Reviews End
