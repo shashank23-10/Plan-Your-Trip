4edb9055caa70cbbb47b3c9bbc9c194b46d98660
Shashank <shashankpandey2001@gmail.com>
2022-12-27 15:12:30 +0530

    faqs

--- myScript.js
+++ myScript.js
@@ -55,6 +55,25 @@ function prev() {
 }
 //Slides for Reviews End
 
+//Frequently Asked Questions//
+const questions = document.querySelectorAll(".question");
+
+questions.forEach(function (question) {
+  const btn = question.querySelector(".question-btn");
+  // console.log(btn);
+
+  btn.addEventListener("click", function () {
+    // console.log(question);
+
+    questions.forEach(function (item) {
+      if (item !== question) {
+        item.classList.remove("show-text");
+      }
+    });
+
+    question.classList.toggle("show-text");
+  });
+});
 //Scroll-Bar
 let progress = document.getElementById("progress-bar");
 let totalHeight = document.body.scrollHeight - window.innerHeight;
